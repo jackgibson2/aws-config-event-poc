86e226838ab09602e4504dc640a028191bc13e52
Jack Gibson <jackgibson2@gmail.com>
2019-11-22 16:09:33 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
   profile    = "default"
   region     = "us-east-1"
+  vpc_id        = "vpc-09ef1a51b6c2f1118"
 }
 
 resource "aws_instance" "example" {
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
-  vpc_id        = "vpc-09ef1a51b6c2f1118"
 }
