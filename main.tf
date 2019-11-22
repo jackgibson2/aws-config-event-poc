e5ce616b962966d60c3b6f191b2f32011c971eab
Jack Gibson <jackgibson2@gmail.com>
2019-11-22 16:04:13 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,9 @@
 provider "aws" {
-  region = "us-east-1"
+  profile    = "default"
+  region     = "us-east-1"
 }
 
 resource "aws_instance" "example" {
- 
-  ami         = "ami-0c09927662c939f41"
+  ami           = "ami-2757f631"
   instance_type = "t2.micro"
- 
-  tags {
- 
-  name = "TESTVM"
- 
-  }
+}
